ac139397499f2d70dc90075460629ec08536eb04
Anthony <99anthonylam@gmail.com>
2019-11-16 17:58:17 -0500

    wip

--- Deploying ML Flask App to GCP/main.py
+++ Deploying ML Flask App to GCP/main.py
@@ -3,16 +3,43 @@ from flask import Flask,render_template,request,url_for
 #EDA Packages
 import pandas as pd
 import numpy as np
+import os
+import psycopg2
+
+db_user = "postgres"
+db_password = "postgres"
+db_name = "postgres"
+db_connection_name = "bostonhacks-259202:us-central1:bh-data"
 
 # ML Packages
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.naive_bayes import MultinomialNB
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	# When deployed to App Engine, the `GAE_ENV` environment variable will be
+	# set to `standard`
+	if os.environ.get('GAE_ENV') == 'standard':
+	# If deployed, use the local socket interface for accessing Cloud SQL
+		host = '/cloudsql/{}'.format(db_connection_name)
+	else:
+	# If running locally, use the TCP connections instead
+	# Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+	# so that your application can use 127.0.0.1:3306 to connect to your
+	# Cloud SQL instance
+		host = '127.0.0.1'
+
+	cnx = psycopg2.connect(dbname=db_name, user=db_user,
+	                   password=db_password, host=host)
+	with cnx.cursor() as cursor:
+		cursor.execute('SELECT * from users;')
+		result = cursor.fetchall()
+	current_time = result[0][0]
+	cnx.commit()
+	cnx.close()
+
+	return (result)
 
 @app.route("/",methods=['POST'])
 def predict():
@@ -43,8 +70,9 @@ def predict():
 		data = [comment]
 		vect = cv.transform(data).toarray()
 		my_prediction = clf.predict(vect)
+
 	return render_template('results.html',prediction = my_prediction,comment = comment)
-	
+
 
 
 if __name__ == '__main__':
