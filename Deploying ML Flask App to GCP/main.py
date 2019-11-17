b004db97a1ecb91bea230ad429bb29a47e1f33d5
Your Name <akriti.bhat28@gmail.com>
2019-11-17 05:49:02 -0500

    Update

--- Deploying ML Flask App to GCP/main.py
+++ Deploying ML Flask App to GCP/main.py
@@ -1,55 +1,39 @@
 import requests
 from flask import Flask,render_template,request,url_for
-
+import json
 import time
-
-#EDA Packages
-import pandas as pd
-import numpy as np
-
-# ML Packages
+import mysql.connector
 
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
-import psycopg2
 
 from libsoundtouch import soundtouch_device
 from libsoundtouch.utils import Source, Type
 
-import pprint
-import sys
-
-import spotipy
-import spotipy.util as util
-
 app = Flask(__name__)
 
+songBeingPlayed = 0
+songPlayedName = "test"
 
 @app.route("/")
 def index():
-	#startDj()
-	#connectToDB()
-	#send_sms('+18572648772')
 	return render_template("index.html")
-
-
-def connectToDB():
-	con = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="34.67.145.3", port="5432")
-
-	print("Database opened successfully")
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-    # Start our response
-    resp = MessagingResponse()
+	mobile_no = request.values.get('From',None)
+	body = request.values.get('Body', None)
+	songId = body.split(" ")[0]
+	bidAmt = body.split(" ")[1]
+	res = update(mobile_no,bidAmt,songId)
+	resp = MessagingResponse()
+	if res :
+		send_sms(mobile_no,"Thank you for your bid!")
+	else :
+		send_sms(mobile_no,"Not Enough Credit!")
 
 
-    # Add a message
-    resp.message("Thank you for your bid!")
-    return str(resp)
-
-def send_sms(number):
+def send_sms(number, messageBody):
 	print(number)
 	account_sid = 'ACf3d029ace0ab2f696f9f971c11696f91'
 	auth_token = '14ac526739b259838c2f67becc772b6f'
@@ -57,35 +41,11 @@ def send_sms(number):
 
 	message = client.messages \
 		.create(
-		body="Your Bidding Won",
+		body=messageBody,
 		from_='+12028312095',
 		to=number
 	)
-
 	print(message.sid)
-
-
-def playSong(track):
-	print("i am playing song")
-	url = "http://192.168.1.168:8090/speaker"
-
-	payload = "<play_info><app_key>L8QYEJtiHzmtZ6EJg8ETr9WB8HbCdYkl</app_key><url>"+track+"</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>50</volume></play_info>"
-	headers = {
-		'Content-Type': "text/plain",
-		'User-Agent': "PostmanRuntime/7.15.0",
-		'Accept': "*/*",
-		'Cache-Control': "no-cache",
-		'Postman-Token': "f9cd60fb-a7f2-45b4-9398-c5ece6112d1d,abb8f174-a27f-410e-9677-22950e096b83",
-		'Host': "192.168.1.168:8090",
-		'accept-encoding': "gzip, deflate",
-		'content-length': "261",
-		'Connection': "keep-alive",
-		'cache-control': "no-cache"
-	}
-
-	response = requests.request("POST", url, data=payload, headers=headers)
-
-	print(response.text)
 
 
 def playSong1(track):
@@ -110,14 +70,11 @@ def getCurrentSongDuration():
 
 
 def startDj():
-
 	i = 0
-	track = getNextSong()
-	playSong1(track)
-
 	while i < 6 :
 		track = getNextSong()
 		playSong1(track)
+		print(currentBid())
 		timeToSleep = getCurrentSongDuration()
 		time.sleep(timeToSleep)
 		i = i+1
@@ -125,7 +82,28 @@ def startDj():
 
 
 def getNextSong():
-	return "0tBbt8CrmxbjRP0pueQkyU"
+	cnx = mysql.connector.connect(user='be3bc40df921df', password='c7708685',
+								  host='us-cdbr-iron-east-04.cleardb.net',
+								  database='heroku_67d188873be555e')
+
+	cursor = cnx.cursor()
+	query = ("select id, song_name, song_id from songs order by bid_amt desc limit 1;")
+	cursor.execute(query)
+
+	for  (id) in cursor:
+		id_temp =  id[2]
+		songPlayedName = id[1]
+		songBeingPlayed = id_temp
+		id_song = id[0]
+
+	BASE_URL = 'delete from songs where id = '
+
+	query2 = (BASE_URL +  str(id_song) +';')
+	cursor.execute(query2)
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+	return id_temp
 
 @app.route("/pauseSong", methods=['GET', 'POST'])
 def pauseSong():
@@ -146,10 +124,72 @@ def pauseSong():
 	}
 
 	response = requests.request("POST", url, data=payload, headers=headers)
-
 	print(response.text)
 
 
+@app.route("/current", methods=['GET', 'POST'])
+def currentSong():
+	return songPlayedName
+
+
+@app.route("/bidData", methods=['GET', 'POST'])
+def currentBid():
+	cnx = mysql.connector.connect(user='be3bc40df921df', password='c7708685',
+								  host='us-cdbr-iron-east-04.cleardb.net',
+								  database='heroku_67d188873be555e')
+
+	cursor = cnx.cursor()
+	query = ("select song_name, bid_amt from songs order by bid_amt desc;")
+	cursor.execute(query)
+
+	songs = []
+	bids = []
+	for (id) in cursor:
+		songs.append(id[0])
+		bids.append(id[1])
+
+	song_json = []
+	for i in range(len(bids)):
+		song_json.append({'name': songs[i], 'bid': bids[i]})
+
+
+	cursor.close()
+	cnx.close()
+	return json.dumps(song_json)
+
+
+def update(phoneNum, bid, song_id):
+	ph = phoneNum
+	ph = ph.replace("+","")
+	cnx = mysql.connector.connect(user='be3bc40df921df', password='c7708685',
+								  host='us-cdbr-iron-east-04.cleardb.net',
+								  database='heroku_67d188873be555e')
+
+	cursor = cnx.cursor()
+	query = ("select id, credits from users where mob_num=" + str(ph) + ";")
+	cursor.execute(query)
+
+	for (id) in cursor:
+		user_id = id[0]
+		user_bid = id[1]
+		if bid > id[0]:
+			return False
+
+	query4 = ("select bid_amt from songs where id=" + str(song_id) + ";")
+	cursor.execute(query4)
+
+	for (id) in cursor:
+		song_bid = id[0]
+
+	query2 = ("update users set credits = " + str(user_bid - bid) + " where id = " + str(user_id) + ";")
+	cursor.execute(query2)
+	query3 = ("update songs set bid_amt = " + str(song_bid + bid) + " where id =" + str(song_id) + ";")
+	cursor.execute(query3)
+
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+	return True
 
 
 if __name__ == '__main__':
